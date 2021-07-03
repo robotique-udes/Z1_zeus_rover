a3d5dc50573893a657d9165638bcf3ffd3bf2902
shovington <samuel.hovington@usherbrooke.ca>
2021-07-03 14:46:53 -0400

    tuning

--- zeus_control/src/twist2motor_cmd.py
+++ zeus_control/src/twist2motor_cmd.py
@@ -35,6 +35,7 @@ class LowLevelControlNode():
         self.r_cmd_outer = 0
         self.last_cmd = [0, 0, 0, 0, 0, 0]
         self.last_cmd_time = [rospy.get_rostime().to_sec()]*6
+        self.last_twist_received = 0.0
 
         # Init publishers
         self.m1_pub = rospy.Publisher('/ros_talon1/motor_percent', Int32, queue_size=10)
@@ -52,9 +53,9 @@ class LowLevelControlNode():
         self.ddr = DDynamicReconfigure("twist2cmd")
 
         # Add variables to ddr(name, description, default value, min, max, edit_method)        
-        self.ddr.add_variable("linear_gain", "float", 0.5, 0, 2)
+        self.ddr.add_variable("linear_gain", "float", 1, 0, 2)
         self.ddr.add_variable("angular_gain", "float", -4, -8, 8)
-        self.ddr.add_variable("max_motor_cmd", "int", 75, 1, 100)
+        self.ddr.add_variable("max_motor_cmd", "int", 100, 1, 100)
         self.ddr.add_variable("max_accel", "float", 100, 1, 300)
         self.ddr.add_variable("outer_wheels_rotation_gain", "float", 1.1, 1.0, 1.5)
 
@@ -106,16 +107,18 @@ class LowLevelControlNode():
             Twist message
         '''
         # Send same command to all motors of same side
-        l_cmd_center = msg.linear.x*75*self.linear_gain + msg.angular.z*30*self.angular_gain
-        r_cmd_center = msg.linear.x*75*self.linear_gain - msg.angular.z*30*self.angular_gain
-        l_cmd_outer = msg.linear.x*75*self.linear_gain + msg.angular.z*30*self.angular_gain*self.outer_wheels_rotation_gain
-        r_cmd_outer = msg.linear.x*75*self.linear_gain - msg.angular.z*30*self.angular_gain*self.outer_wheels_rotation_gain
+        l_cmd_center = -msg.linear.x*100*self.linear_gain - msg.angular.z*30*self.angular_gain
+        r_cmd_center = msg.linear.x*100*self.linear_gain - msg.angular.z*30*self.angular_gain
+        l_cmd_outer = -msg.linear.x*100*self.linear_gain - msg.angular.z*30*self.angular_gain*self.outer_wheels_rotation_gain
+        r_cmd_outer = msg.linear.x*100*self.linear_gain - msg.angular.z*30*self.angular_gain*self.outer_wheels_rotation_gain
 
         # Limit speed 
         self.l_cmd_center = self.limit_speed(l_cmd_center, max_val=self.max_motor_cmd)
         self.r_cmd_center = self.limit_speed(r_cmd_center, max_val=self.max_motor_cmd)
         self.l_cmd_outer = self.limit_speed(l_cmd_outer, max_val=self.max_motor_cmd)
         self.r_cmd_outer = self.limit_speed(r_cmd_outer, max_val=self.max_motor_cmd)
+
+        self.last_twist_received = rospy.get_rostime().to_sec()
 
 
     def limit_speed(self, cmd, max_val=60):
@@ -162,19 +165,31 @@ class LowLevelControlNode():
         self.send_cmd()
 
 
+
     def send_cmd(self):
         '''
         Publishes commands
         '''
-        # Left wheels
-        self.m1_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_outer*self.wheel_1_gain, max_val=100), 0))
-        self.m3_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_center*self.wheel_3_gain, max_val=100), 2))
-        self.m5_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_outer*self.wheel_5_gain, max_val=100), 4))
-        
-        # Right wheels
-        self.m2_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_outer*self.wheel_2_gain, max_val=100), 1))
-        self.m4_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_center*self.wheel_4_gain, max_val=100), 3))
-        self.m6_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_outer*self.wheel_6_gain, max_val=100), 5))
+        if self.last_twist_received + 0.5 > rospy.get_rostime().to_sec():
+            # Left wheels
+            self.m1_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_outer*self.wheel_1_gain, max_val=100), 0))
+            self.m3_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_center*self.wheel_3_gain, max_val=100), 2))
+            self.m5_pub.publish(self.limit_accel(self.limit_speed(self.l_cmd_outer*self.wheel_5_gain, max_val=100), 4))
+            
+            # Right wheels
+            self.m2_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_outer*self.wheel_2_gain, max_val=100), 1))
+            self.m4_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_center*self.wheel_4_gain, max_val=100), 3))
+            self.m6_pub.publish(self.limit_accel(self.limit_speed(self.r_cmd_outer*self.wheel_6_gain, max_val=100), 5))
+        else:
+            # Left wheels
+            self.m1_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 0))
+            self.m3_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 2))
+            self.m5_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 4))
+            
+            # Right wheels
+            self.m2_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 1))
+            self.m4_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 3))
+            self.m6_pub.publish(self.limit_accel(self.limit_speed(0, max_val=100), 5))
 
 
     def on_shutdown(self):
