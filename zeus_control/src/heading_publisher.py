eb22b6f8e17f0553fdfaf534befa3ad1aa1da4c0
shovington <samuel.hovington@usherbrooke.ca>
2021-07-24 22:27:31 -0400

    fix heading publisher

--- zeus_control/src/heading_publisher.py
+++ zeus_control/src/heading_publisher.py
@@ -18,8 +18,9 @@ from math import sin, cos, pi
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Imu
-
+from sensor_msgs.msg import Imu, NavSatFix
+from gps_common.msg import GPSFix
+import tf
 
 class HeadingPublisher():
     def __init__(self):
@@ -32,21 +33,53 @@ class HeadingPublisher():
 
         # Subscriber
         ## subscribe to motor's encoder
-        rospy.Subscriber("/imu/data", Imu, self.imuCallback)
+        rospy.Subscriber("/imu/data_out", Imu, self.imuCallback)
+        rospy.Subscriber("/fix", NavSatFix, self.gpsCallback)
 
         # Publisher
-        self.headingPub = rospy.Publisher("/heading", PoseStamped, queue_size=10)
-        # self.odomBroadcaster = TransformBroadcaster()
+        self.gpsPub = rospy.Publisher("/fix_out", GPSFix, queue_size=10)
+
+        rospy.Timer(rospy.Duration(1.0/5.0), self.timerCallback)
+
+        self.gps_in = NavSatFix()
+        self.imu = Imu()
+        self.gps_out = GPSFix()
 
 
     def imuCallback(self, msg):
-        heading = PoseStamped()
-        heading.header = msg.header
-        heading.header.frame_id = "/map"
-        heading.pose.orientation = msg.orientation
-        self.headingPub.publish(heading)
+        self.imu.header = msg.header
+        self.imu.orientation = msg.orientation
 
+    def gpsCallback(self, msg):
+        self.gps_in.header = msg.header
+        self.gps_in.status = msg.status
+        self.gps_in.latitude = msg.latitude
+        self.gps_in.longitude = msg.longitude
+        self.gps_in.altitude = msg.altitude
+        self.gps_in.position_covariance = msg.position_covariance
+        self.gps_in.position_covariance_type = msg.position_covariance_type
 
+    def timerCallback(self, evt):
+        self.gps_out.header = self.gps_in.header
+        self.gps_out.header.stamp = rospy.Time.now()
+        self.gps_out.latitude = self.gps_in.latitude
+        self.gps_out.longitude = self.gps_in.longitude
+        self.gps_out.altitude = self.gps_in.altitude
+        self.gps_out.roll, self.gps_out.pitch, self.gps_out.track  = self.quat2yaw(self.imu.orientation)
+        self.dip = self.gps_out.track
+        self.gpsPub.publish(self.gps_out)
+
+    def quat2yaw(self, quat):
+        quaternion = (
+        quat.x,
+        quat.y,
+        quat.z,
+        quat.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        roll = euler[0]
+        pitch = euler[1]
+        yaw = euler[2]*180/3.14159
+        return roll, pitch, yaw
 
     def on_shutdown(self):
         pass
